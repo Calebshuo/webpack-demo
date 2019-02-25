46db66a8b21bb5fef112cd51022d2d6b24073459
li.shuo <444947389@qq.com>
2019-02-25 16:56:37 +0800

    getting start and file-loader to resolve img and use placeholder

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,13 @@ module.exports = {
       // },
       {
          test: /\.(png|svg|jpg|gif)$/,
-         use: [
-           'file-loader'
-         ]
+         use: {
+           loader: 'file-loader',
+           options: {
+             // placeholder
+             name: '[name]_[hash].[ext]'
+           }
+         }
        }
     ]
   }
